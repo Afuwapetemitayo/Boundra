801929aa4f0cc8a8c36f8dbc72bc843e489a5993
Afuwape Temitayo <temmyteegraphic@gmail.com>
2026-06-28 23:03:31 +0100

    commented pricing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import ProjectDetail from './pages/ProjectDetail'
-import Pricing from './pages/Pricing'
+// import Pricing from './pages/Pricing'
 import Contact  from './pages/Contact'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/project/:id" element={<ProjectDetail />} />
-      <Route path="/pricing" element={<Pricing standalone={true} />} />
+      {/* <Route path="/pricing" element={<Pricing standalone={true} />} /> */}
       <Route path="/contact" element={<Contact />} />
     </Routes>
   )
